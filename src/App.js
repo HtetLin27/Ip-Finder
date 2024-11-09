334c73c8b20df383e11b61c1a73ffc7a6b537e95
htetlynn <htetlynnko27@gmail.com>
2024-11-09 17:34:41 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Map from "./components/Map";
 
 function App() {
+  const [ipDetails, setIpDetails] = useState({});
+  const [lat, setLat] = useState(""); // Default latitude
+  const [lon, setLon] = useState(""); // Default longitude
+
+  useEffect(() => {
+    axios.get("https://ipapi.co/json/").then((res) => {
+      setIpDetails(res.data);
+      setLat(res.data.latitude);
+      setLon(res.data.longitude);
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="heading">IP Address Finder</h1>
+      <div className="App">
+        <div className="left">
+          <h4>What is my IPv4 address?</h4>
+          <h1 id="ip">{ipDetails.ip}</h1>
+          <h4>Approximate location: </h4>
+
+          <p>
+            {ipDetails.city}, {ipDetails.region},{ipDetails.country_name}.
+          </p>
+
+          <h4>Internet Service Provider(ISP):</h4>
+
+          <p>{ipDetails.org}</p>
+        </div>
+        {lon && <Map lat={lat} lon={lon} />}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Map.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+const Map = ({ lat, lon }) => {
+  const customMarker = new L.Icon({
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [0, -41],
+  });
+  return (
+    <MapContainer
+      center={[lat, lon]}
+      zoom={13}
+      style={{ width: "600px", height: "400px" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[lat, lon]} icon={customMarker}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
